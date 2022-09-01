ee3205a8a5011e4d559e23613093b1f1e079936b
maymhuekoko <maymyatmoe211099@gmail.com>
2022-09-01 13:30:21 +0630

    second

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -507,7 +507,7 @@
 <script src="{{asset('dist/js/adminlte.js')}}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <!-- <script src="{{asset('dist/js/pages/dashboard.js')}}"></script> -->
-<!-- AdminLTE for demo purposes -->s
+<!-- AdminLTE for demo purposes -->
 <script src="{{asset('dist/js/demo.js')}}"></script>
 <!-- Bootstrap 4 -->
 <!-- DataTable -->
